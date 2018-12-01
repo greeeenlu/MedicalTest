75a6278fb829f47aac17aad5bc8ea75a2165798d
green.lu <green.lu@titansoft.com.sg>
2018-12-01 17:27:38 +0800

    mapper

---
+++ MedicalTest/Bet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MedicalTest
+{
+    internal class Bet
+    {
+        public Bet()
+        {
+        }
+
+        public int Id { get; set; }
+        public double Stake { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ MedicalTest/BetTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MedicalTest
+{
+    [TestFixture]
+    public class BetTest
+    {
+        [Test]
+        public void TransferBet()
+        {
+            Bet bet = new Bet() { Id = 1, Stake = 344.4, CreateDate = new DateTime(2018, 12, 1) };
+
+            BetDto betDto = Mapper.TransferBet<Bet, BetDto>(bet, b => new BetDto()
+            {
+                BetId = b.Id,
+                Date = b.CreateDate.ToString("yyyy-MM-dd"),
+                Amount = (int)b.Stake
+            });
+
+            Assert.AreEqual(1, betDto.BetId);
+            Assert.AreEqual("2018-12-01", betDto.Date);
+            Assert.AreEqual(344, betDto.Amount);
+        }
+    }
+
+    public class Mapper
+    {
+        public static TResult TransferBet<TSource, TResult>(TSource bet, Func<TSource, TResult> map)
+        {
+            return map(bet);
+        }
+    }
+
+    internal class BetDto
+    {
+        public int Amount { get; set; }
+
+        public string Date { get; set; }
+
+        public int BetId { get; set; }
+    }
+}
